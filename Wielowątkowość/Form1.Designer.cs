79ba8a8d09732d9d84ce645563cbddb566f40272
Piotr Bączkiewicz <piotrbaczkiewicz1tbs@gmail.com>
2023-04-14 20:20:29 +0200

    Dodaj pliki projektów.

---
+++ Wielowątkowość/Form1.Designer.cs
@@ -0,0 +1,142 @@
+﻿namespace Wielowątkowość
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Wymagana zmienna projektanta.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Wyczyść wszystkie używane zasoby.
+        /// </summary>
+        /// <param name="disposing">prawda, jeżeli zarządzane zasoby powinny zostać zlikwidowane; Fałsz w przeciwnym wypadku.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Kod generowany przez Projektanta formularzy systemu Windows
+
+        /// <summary>
+        /// Metoda wymagana do obsługi projektanta — nie należy modyfikować
+        /// jej zawartości w edytorze kodu.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.label1 = new System.Windows.Forms.Label();
+            this.numericUpDown = new System.Windows.Forms.NumericUpDown();
+            this.button = new System.Windows.Forms.Button();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.progressBar = new System.Windows.Forms.ProgressBar();
+            this.labelProgress = new System.Windows.Forms.Label();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(105, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Długość permutacji :";
+            // 
+            // numericUpDown
+            // 
+            this.numericUpDown.Location = new System.Drawing.Point(123, 7);
+            this.numericUpDown.Maximum = new decimal(new int[] {
+            6,
+            0,
+            0,
+            0});
+            this.numericUpDown.Minimum = new decimal(new int[] {
+            3,
+            0,
+            0,
+            0});
+            this.numericUpDown.Name = "numericUpDown";
+            this.numericUpDown.Size = new System.Drawing.Size(40, 20);
+            this.numericUpDown.TabIndex = 1;
+            this.numericUpDown.Value = new decimal(new int[] {
+            3,
+            0,
+            0,
+            0});
+            // 
+            // button
+            // 
+            this.button.Location = new System.Drawing.Point(182, 7);
+            this.button.Name = "button";
+            this.button.Size = new System.Drawing.Size(75, 23);
+            this.button.TabIndex = 2;
+            this.button.Text = "Generuj";
+            this.button.UseVisualStyleBackColor = true;
+            this.button.Click += new System.EventHandler(this.button_Click);
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Location = new System.Drawing.Point(17, 54);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.ReadOnly = true;
+            this.richTextBox1.Size = new System.Drawing.Size(445, 317);
+            this.richTextBox1.TabIndex = 3;
+            this.richTextBox1.Text = "";
+            // 
+            // progressBar
+            // 
+            this.progressBar.Location = new System.Drawing.Point(12, 415);
+            this.progressBar.Name = "progressBar";
+            this.progressBar.Size = new System.Drawing.Size(450, 23);
+            this.progressBar.TabIndex = 4;
+            // 
+            // labelProgress
+            // 
+            this.labelProgress.AutoSize = true;
+            this.labelProgress.Location = new System.Drawing.Point(14, 389);
+            this.labelProgress.Name = "labelProgress";
+            this.labelProgress.Size = new System.Drawing.Size(70, 13);
+            this.labelProgress.TabIndex = 5;
+            this.labelProgress.Text = "labelProgress";
+            // 
+            // timer1
+            // 
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(474, 450);
+            this.Controls.Add(this.labelProgress);
+            this.Controls.Add(this.progressBar);
+            this.Controls.Add(this.richTextBox1);
+            this.Controls.Add(this.button);
+            this.Controls.Add(this.numericUpDown);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Wielowątkowość";
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.NumericUpDown numericUpDown;
+        private System.Windows.Forms.Button button;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.ProgressBar progressBar;
+        private System.Windows.Forms.Label labelProgress;
+        private System.Windows.Forms.Timer timer1;
+    }
+}
+
---
+++ Wielowątkowość/Form1.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Threading;
+using System.Windows.Forms;
+
+namespace Wielowątkowość
+{
+    public partial class Form1 : Form
+    {
+        string printValue;
+        int numbers;
+        int values=1;
+        int startValues = 1;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button_Click(object sender, EventArgs e)
+        {
+
+
+            startValues = 1;
+            numbers = (int)numericUpDown.Value;
+            for (int i = 0; i < numbers; i++)
+            {
+                startValues *= 10;
+            }
+            values = startValues;
+            //MessageBox.Show(values.ToString());
+            timer1.Interval = 100;
+            progressBar.Maximum = values;
+            
+            
+            timer1.Start();
+            
+            
+
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            
+            printValue = values.ToString();
+            for (int j = 1; j < numbers + 1; j++)
+            {
+                richTextBox1.AppendText(printValue[j].ToString());
+            }
+            progressBar.Value = (values - startValues)+1;
+            labelProgress.Text = "Permutacja : " + ((values - startValues)+1).ToString() +"/"+startValues.ToString();
+            richTextBox1.AppendText("\n");
+            values++;
+            if (values > startValues * 2-1)
+            {
+                timer1.Stop();
+            }
+
+        }
+    }
+}
